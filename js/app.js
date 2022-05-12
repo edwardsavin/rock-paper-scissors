08d74f5dee3078806871bf80e59fe9111598780d
Edward Savin <github@edwardcs.com>
2022-05-13 02:14:31 +0300

    Add playRound() function
    The playRound() function sets the string comparison (ex. paper > rock),
    compares the playerSelection against computerSelection and prints the
    winner.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// TODO: uppercase first letter of playerSelection and change all the other rock/ paper/ scissors strings to title case
+
 // List variable with choices
 let choices = ["rock", "paper", "scissors"];
 
@@ -6,3 +8,37 @@ let computerSelection = choices[Math.floor(Math.random() * choices.length)];
 
 // Variable for human (playerSelection) with prompt
 let playerSelection = prompt("Rock, Paper or Scissors?").trim().toLowerCase();
+
+// Function (playRound()) that declares the comparison of the strings (rock > scissors, etc.),
+// compares playerSelection with computerSelection, prints the winner and
+// TODO: returns it
+function playRound() {
+    "rock" > "scissors";
+    "rock" < "paper";
+    "paper" < "scissors";
+
+    if (
+        playerSelection === "rock" ||
+        playerSelection === "paper" ||
+        playerSelection === "scissors"
+    ) {
+        if (playerSelection > computerSelection) {
+            console.log(
+                `You Win! ${playerSelection} beats ${computerSelection}`
+            );
+        } else if (playerSelection < computerSelection) {
+            console.log(
+                `You Lose! ${computerSelection} beats ${playerSelection}`
+            );
+        } else if (playerSelection === computerSelection) {
+            console.log(`It's a tie! You both chose ${playerSelection}`);
+        } else {
+            console.log("Something went wrong...");
+        }
+    } else {
+        console.log(`You cannot choose ${playerSelection}`);
+    }
+}
+
+playRound();
+// TODO: Function (game()) that replays the game for 5 times (recalls playRound() for 5 times)
