0b304ba38a4427fcfa1ee8c40ffa5878f9dbe56a
Edward Savin <github@edwardcs.com>
2022-05-13 01:48:51 +0300

    Modify name of human variable -> playerSelection

--- js/app.js
+++ js/app.js
@@ -3,9 +3,6 @@ let choices = ["rock", "paper", "scissors"];
 
 // Variable for computer that randomly chooses from choices list
 let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-console.log(computerSelection);
 
 // Variable for human (playerSelection) with prompt
-let human = prompt("Rock, Paper or Scissors?").trim().toLowerCase();
-console.log(human);
-
+let playerSelection = prompt("Rock, Paper or Scissors?").trim().toLowerCase();
