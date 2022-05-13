d830b1ced0cc73c118b86f6f38541bd6b5405d04
Edward Savin <github@edwardcs.com>
2022-05-13 22:05:15 +0300

    Add upperCaseFirstChar() function

--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
-// TODO: uppercase first letter of playerSelection and change all the other rock/ paper/ scissors strings to title case
-
 // List variable with choices
-let choices = ["rock", "paper", "scissors"];
+let choices = ["Rock", "Paper", "Scissors"];
 
 // Variable for computer that randomly chooses from choices list
 let computerSelection = choices[Math.floor(Math.random() * choices.length)];
 
 // Variable for human (playerSelection) with prompt
-let playerSelection = prompt("Rock, Paper or Scissors?").trim().toLowerCase();
+let playerSelection = upperCaseFirstChar(
+    prompt("Rock, Paper or Scissors?").trim().toLowerCase()
+);
 
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
 
-// Function (playRound()) that declares the comparison of the strings (rock > scissors, etc.),
-// compares playerSelection with computerSelection, prints the winner and
-// TODO: returns it
+// Function (playRound()) that declares the comparison of the strings (Rock > Scissors, etc.),
+// compares playerSelection with computerSelection, prints the winner and returns it
 function playRound() {
-    "rock" > "scissors";
-    "rock" < "paper";
-    "paper" < "scissors";
+    "Rock" > "Scissors";
+    "Rock" < "Paper";
+    "Paper" < "Scissors";
 
     if (
-        playerSelection === "rock" ||
-        playerSelection === "paper" ||
-        playerSelection === "scissors"
+        playerSelection === "Rock" ||
+        playerSelection === "Paper" ||
+        playerSelection === "Scissors"
     ) {
         if (playerSelection > computerSelection) {
             console.log(
                 `You Win! ${playerSelection} beats ${computerSelection}`
             );
-            return playerScore += 1
+            return (playerScore += 1);
         } else if (playerSelection < computerSelection) {
             console.log(
                 `You Lose! ${computerSelection} beats ${playerSelection}`
             );
-            return computerScore += 1
+            return (computerScore += 1);
         } else if (playerSelection === computerSelection) {
             console.log(`It's a tie! You both chose ${playerSelection}`);
         } else {
@@ -45,6 +44,11 @@ function playRound() {
     }
 }
 
+function upperCaseFirstChar(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
 playRound();
-console.log(`The score is Player: ${playerScore} | Computer: ${computerScore}`)
+
+console.log(`The score is Player: ${playerScore} | Computer: ${computerScore}`);
 // TODO: Function (game()) that replays the game for 5 times (recalls playRound() for 5 times)
