d4594e150a17a32d877eb788fcff71b3266f3a75
Edward Savin <github@edwardcs.com>
2022-05-20 01:25:03 +0300

    Add starting point UI and refactor the code to work with the UI

--- js/app.js
+++ js/app.js
@@ -1,73 +1,70 @@
 let playerScore = 0;
 let computerScore = 0;
 
-// Play one round of the game and return the winner
-function playRound() {
-    const choices = ["Rock", "Paper", "Scissors"];
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
 
-    // Make computer select randomly from choices and prompt user for selection
-    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-    let playerSelection = upperCaseFirstChar(
-        prompt("Rock, Paper or Scissors?").trim().toLowerCase()
-    );
+rock.addEventListener("click", selectRock);
+paper.addEventListener("click", selectPaper);
+scissors.addEventListener("click", selectScissors);
 
-    // Declare the comparison between the strings
-    "Rock" > "Scissors";
-    "Rock" < "Paper";
-    "Paper" < "Scissors";
+function selectRock() {
+  playRound("Rock");
+}
 
-    // Compare playerSelection with computerSelection, print winner and return it
-    if (
-        playerSelection === "Rock" ||
-        playerSelection === "Paper" ||
-        playerSelection === "Scissors"
-    ) {
-        if (playerSelection > computerSelection) {
-            console.log(
-                `You Win! ${playerSelection} beats ${computerSelection}`
-            );
-            return (playerScore += 1);
-        } else if (playerSelection < computerSelection) {
-            console.log(
-                `You Lose! ${computerSelection} beats ${playerSelection}`
-            );
-            return (computerScore += 1);
-        } else if (playerSelection === computerSelection) {
-            console.log(`It's a tie! You both chose ${playerSelection}`);
-        } else {
-            console.log("Something went wrong...");
-        }
+function selectPaper() {
+  playRound("Paper");
+}
+
+function selectScissors() {
+  playRound("Scissors");
+}
+
+function playRound(buttonSelected) {
+  const choices = ["Rock", "Paper", "Scissors"];
+
+  let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+  let playerSelection = buttonSelected;
+
+  "Rock" > "Scissors";
+  "Rock" < "Paper";
+  "Paper" < "Scissors";
+
+  // Compare playerSelection with computerSelection, show the score and display round result
+  // Execute only if the player or computer score is < 5
+  if (playerScore < 5 && computerScore < 5) {
+    if (playerSelection > computerSelection) {
+      playerScore += 1;
+      document.getElementById(
+        "round-result"
+      ).textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection < computerSelection) {
+      computerScore += 1;
+      document.getElementById(
+        "round-result"
+      ).textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === computerSelection) {
+      document.getElementById(
+        "round-result"
+      ).textContent = `It's a tie! You both chose ${playerSelection}`;
     } else {
-        console.log(`You cannot choose ${playerSelection}`);
+      document.getElementById("round-result").textContent =
+        "Something went wrong...";
     }
+
+    announceWinner();
+  }
+
+  document.getElementById(
+    "match-score"
+  ).textContent = `Match score is: [ Player: ${playerScore} | Computer: ${computerScore} ]`;
 }
 
-// Upper case only the first character in string
-function upperCaseFirstChar(s) {
-    return s.charAt(0).toUpperCase() + s.slice(1);
+function announceWinner() {
+  if (playerScore === 5) {
+    document.getElementById("game-winner").textContent = "You won the game!";
+  } else if (computerScore === 5) {
+    document.getElementById("game-winner").textContent = "You lost the game!";
+  }
 }
-
-// Play the game for five rounds and at the end declare the winner
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
-    if (playerScore > computerScore) {
-        console.log("Game won!");
-        console.log(
-            `Match score is: [ Player: ${playerScore} | Computer: ${computerScore} ]`
-        );
-    } else if (playerScore < computerScore) {
-        console.log("Game lost!");
-        console.log(
-            `Match score is: [ Player: ${playerScore} | Computer: ${computerScore} ]`
-        );
-    } else {
-        console.log("The game ended in a draw!");
-        console.log(
-            `Match score is: [ Player: ${playerScore} | Computer: ${computerScore} ]`
-        );
-    }
-}
-
-game();
