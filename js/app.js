9ac8697ecd6c821faa0086c7aa7451fca3cfb252
Edward Savin <github@edwardcs.com>
2022-05-20 18:14:10 +0300

    Solve choice comparison bug and add update UI

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
+const score = document.getElementById("match-score");
+const roundResult = document.getElementById("round-result");
+const gameResult = document.getElementById("game-winner");
+
 rock.addEventListener("click", selectRock);
 paper.addEventListener("click", selectPaper);
 scissors.addEventListener("click", selectScissors);
@@ -27,44 +31,39 @@ function playRound(buttonSelected) {
   let computerSelection = choices[Math.floor(Math.random() * choices.length)];
   let playerSelection = buttonSelected;
 
-  "Rock" > "Scissors";
-  "Rock" < "Paper";
-  "Paper" < "Scissors";
-
   // Compare playerSelection with computerSelection, show the score and display round result
   // Execute only if the player or computer score is < 5
   if (playerScore < 5 && computerScore < 5) {
-    if (playerSelection > computerSelection) {
+    if (
+      (playerSelection === "Rock" && computerSelection === "Scissors") ||
+      (playerSelection === "Paper" && computerSelection === "Rock") ||
+      (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
       playerScore += 1;
-      document.getElementById(
-        "round-result"
-      ).textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection < computerSelection) {
+      roundResult.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (
+      (playerSelection === "Rock" && computerSelection === "Paper") ||
+      (playerSelection === "Paper" && computerSelection === "Scissors") ||
+      (playerSelection === "Scissors" && computerSelection === "Rock")
+    ) {
       computerScore += 1;
-      document.getElementById(
-        "round-result"
-      ).textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+      roundResult.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection === computerSelection) {
-      document.getElementById(
-        "round-result"
-      ).textContent = `It's a tie! You both chose ${playerSelection}`;
+      roundResult.textContent = `It's a tie! You both chose ${playerSelection}`;
     } else {
-      document.getElementById("round-result").textContent =
-        "Something went wrong...";
+      roundResult.textContent = "Something went wrong...";
     }
 
     announceWinner();
   }
 
-  document.getElementById(
-    "match-score"
-  ).textContent = `Match score is: [ Player: ${playerScore} | Computer: ${computerScore} ]`;
+  score.textContent = `Match score is: [ Player: ${playerScore} | Computer: ${computerScore} ]`;
 }
 
 function announceWinner() {
   if (playerScore === 5) {
-    document.getElementById("game-winner").textContent = "You won the game!";
+    gameResult.textContent = "You won the game!";
   } else if (computerScore === 5) {
-    document.getElementById("game-winner").textContent = "You lost the game!";
+    gameResult.textContent = "You lost the game!";
   }
 }
